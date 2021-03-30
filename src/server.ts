a35b24cfce97b326deb55eb1a82c2dcaeed25f77
José <josevictortf@gmail.com>
2021-03-30 14:23:26 -0300

    Arquivos iniciais

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { app } from ".";
+import { AddressInfo } from "net";
+
+const server = app.listen(3333, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
